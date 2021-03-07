d53a8d531688a918b455b7bf1d8145bd9127d289
HybridSquirrel <robert.squirrel@mailfence.com>
2021-03-07 14:57:36 +0100

    configureated working enviorment

---
+++ cmake-build-debug/CMakeFiles/AmbiencePlayer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "AmbiencePlayer"
+  "AmbiencePlayer.pdb"
+  "CMakeFiles/AmbiencePlayer.dir/ambienceplayer.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/AmbiencePlayer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
